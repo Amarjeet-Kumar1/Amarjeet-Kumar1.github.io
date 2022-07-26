6541d9f941d73df71b67e53de3460bc0702cc973
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2022-07-26 18:26:53 +0530

    final js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// back to top animation 
+
 var backTop = document.getElementById('back-top');
 backTop.addEventListener('click', function(){
     var initialPos = window.pageYOffset;
@@ -15,6 +17,8 @@ backTop.addEventListener('click', function(){
 }
 });
 
+
+// smooth scroll animation 
 
 var navList = document.querySelectorAll('.nav-menu a');
 var id;
@@ -40,6 +44,56 @@ function scrollDiv(sectionTargeted){
     }
     window.scrollBy(0,50);
 }
+
+// skill progress animation
+
+var progress = document.getElementsByClassName('per');
+
+function initialiseBar(bar){
+    bar.setAttribute("data-visited", false);
+    bar.style.width = 0 + '%';
+}
+
+for (var bar of progress){
+    initialiseBar(bar);
+}
+
+function fillBar(bar){
+    var currentWidth = 0;
+    var targetWidth = bar.getAttribute("data-progress");
+    var interval = setInterval(function(){
+        if(currentWidth >= targetWidth){
+            clearInterval(interval);
+            return;
+        }
+        currentWidth++;
+        bar.style.width = currentWidth + '%';
+    }, 5);
+}
+
+function isBarVisible(){
+    for(let bar of progress){
+        var barCoordinates = bar.getBoundingClientRect();
+        if((bar.getAttribute("data-visited") == "false") && 
+        (barCoordinates.top <= (window.innerHeight - barCoordinates.height)) && 
+        (barCoordinates.top >0)){
+            bar.setAttribute("data-visited", true);
+            fillBar(bar);
+        } else if((barCoordinates.top > window.innerHeight) || (barCoordinates.bottom < 0)){
+            bar.setAttribute("data-visited", false);
+            initialiseBar(bar);
+        }
+    }
+}
+
+window.onscroll = function(){
+    isBarVisible();
+    }
+
+
+
+
+
 
 
 
